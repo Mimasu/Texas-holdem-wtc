cf8463b30642f4053afe60b4414c857a171d4d64
Jamie Irvine <jirvine@students.inghamisd.org>
2018-05-08 10:23:29 -0400

    made some major updates to identification of players

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList; //needed for lists
 public class Main {
 
 
-    //currently this code creates two lists, one of integers based on the amount of players, and one with Players instead of integers
+    //currently this code creates two lists, one of integers based on the amount of players, and one with Players instead of integers and is ready to call to the methods
     public static void main(String[] args) {
         Scanner scannername = new Scanner (System.in);
         Deck gameDeck = new Deck();  //game deck is the object that will be modified in the cases of dealing and creating the community cards
@@ -55,34 +55,55 @@ public class Main {
                     intplayerlist.set((isaDealer + c),2);//sets the big blind to be 2
                     System.out.println("the dealer button is in the position of player " + (isaDealer + 1) + " for this hand");
                     System.out.println("the small blind for this hand is player " + (isaDealer + b + 1));
+                    int smallblind = (isaDealer + b + 1);
                     System.out.println("the big blind for this hand is player " + (isaDealer + c + 1));
                     System.out.println(intplayerlist);// this will do the same but neither of these are necessary it's just for debugging purposes
                     System.out.println("players, continue code from here");
                     isaDealer = isaDealer - 1; // moves the dealer button clockwise each hand ... don't let this become a negative!
                     if(isaDealer < 1){
                         isaDealer = numberofPlayers-1; //subtract 1 because elements do funny things
+                        //include dealing of cards
+                        String handloop = "yes";
+                        int timesthrough = 0;
+                        System.out.println(smallblind);
+
+
+
+                        /*
+                        We need two integers for the player class: Jamie's idea for identifying Players
+                        int PlayerID; // this would be used to identify the player's element value (these values will be declared as the Players are declared)
+                        int PlayerStat = intplayerlist(PlayerID); // this would be used to know what the player's current role is
+                         */
+                        //Player.deal(); // the order shoulden't matter, as long as each player gets cards and these cards remain private until showdown
+
                         System.out.println("Small blind player, choose your move" );
                         String action = scannername.nextLine();
                         if (action.equals("raise")){
                             System.out.println("Player " + 1 + " raises");
                             //
                             //call to the raises method for player
+                            //(playerlist.get((smallblind))).raise(); // Player actually means the object name of the small blind player (there are two seperate lists for this :Thinking:)
                         }
                         else if (action.equals("bet")){
                             System.out.println("Player " + 1 + " bets");
                             //meaning giving money to the pit
                             //call to bet method for player
+                            //Player.bet();
                         }
                         else if (action.equals("hold")){
                             System.out.println("Player " + 1 + " holds");
-                            //meaning that this player is not gonna bet and can not win the pot
+                            //meaning that this player is not gonna bet and can not win the pot and will be skipped over during this hand
                             //call to hold method for player
+                            //Player.hold();
                         }
                         else if (action.equals("all in")){
                             System.out.println("Player " + 1 + " goes all in");
                             //meaning that this player is putting all their chips in the pit
                             //call to all in method for player
+                            //Player.allin();
                         }
+                        timesthrough = timesthrough + 1; //this is gonna be used to change the current player being called at
+                        handloop = "yes";
                         //call to dealing method where : private cards to each player > Small blind player one > Big blind one last > until each player has 2 cards > deck size changed (this should require an automated process and should not be ai driven unless we want to add the possibility of the dealer having dealing errors)
                         //enter bidding round
                         //at the end of hand..
