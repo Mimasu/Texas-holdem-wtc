a938b9314fdfcf54927589d3494195a6004cfe70
Jamie Irvine <jirvine@students.inghamisd.org>
2018-05-09 10:22:22 -0400

    updates to identification of players, added some things to the player class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,7 +21,7 @@ public class Main {
                 int fromzero = 0;
                 int playernumber = 1;
                 ArrayList<Player> playerlist = new ArrayList<Player>(); //the element value represents the objects of the player class
-                ArrayList<Integer> intplayerlist = new ArrayList<Integer>();  //the element number represents the player player 1 is element 0
+                ArrayList<Integer> intplayerlist = new ArrayList<Integer>();  //the element number represents the player player 1 is element 0 -----------------------------------------------------------------------------------------------------------------
                 while(fromzero < numberofPlayers){
                     playerlist.add(new Player(playernumber)); //i'm pretty sure I want to have each player be of the player class but it might work to have two list of players one for player class and one for main class to change the deciding factors for game things like the blinds and stuff
                     intplayerlist.add(playernumber); //the second list for main to get numbers that target other numbers or something...
@@ -57,6 +57,7 @@ public class Main {
                     System.out.println("the small blind for this hand is player " + (isaDealer + b + 1));
                     int smallblind = (isaDealer + b + 1);
                     System.out.println("the big blind for this hand is player " + (isaDealer + c + 1));
+                    int bigblind = (isaDealer + c + 1); // we probably don't need this variable
                     System.out.println(intplayerlist);// this will do the same but neither of these are necessary it's just for debugging purposes
                     System.out.println("players, continue code from here");
                     isaDealer = isaDealer - 1; // moves the dealer button clockwise each hand ... don't let this become a negative!
@@ -64,46 +65,91 @@ public class Main {
                         isaDealer = numberofPlayers-1; //subtract 1 because elements do funny things
                         //include dealing of cards
                         String handloop = "yes";
-                        int timesthrough = 0;
-                        System.out.println(smallblind);
-
-
-
+                        String areAllFolded = "no";
+                        String hasallmatchedpot = "no";
+                        String doPlayersStillHaveChips = "yes";
+                        int timesthrough = 0; // this should become zero at the start of every hand as this represents the amount of times through the hand
+                        int activePlayer;
+                        while(handloop.equals("yes")){
+                        //System.out.println(smallblind);
                         /*
                         We need two integers for the player class: Jamie's idea for identifying Players
                         int PlayerID; // this would be used to identify the player's element value (these values will be declared as the Players are declared)
                         int PlayerStat = intplayerlist(PlayerID); // this would be used to know what the player's current role is
+                        the playerStat will declare in the player class what the player can do or not.
+                        String hasplayerfolded; // this will be used to declare if the defined player has folded
+                        String potmatch; // this will be used to declare if the defined player has matched the amount each player put in the pot, if player is not active set this to be yes
+                        String playeractive;
+                        int chipsLeft; //used to determine if the defined player still has chips/$
                          */
-                        //Player.deal(); // the order shoulden't matter, as long as each player gets cards and these cards remain private until showdown
+                        /*
+                        now I need to code a way to make the program skip players who are not active in the game so they don't interrupt the players that can play
+                         */
+                            //Player.deal(); // the order shoulden't matter, as long as each player gets cards and these cards remain private until showdown
+                            activePlayer = (smallblind - 1 + timesthrough);
+                            if (activePlayer > numberofPlayers - 1 ){// - 1 because if active player is equal to number of players than it will go out of bounds
+                                activePlayer = 0 + timesthrough - 1; // 0 so the order doesn't get messed up, this should start to happen after the last player in the list does their action
+                            }
+                            System.out.println(activePlayer + " is the active player...");
+                            int visualactiveP = activePlayer +1;
+                            //if (visualactiveP > numberofPlayers){
+                           //     visualactiveP = 1;
+                            //}
+                            System.out.println("Player " + (visualactiveP) + ", choose your move" );
+                            String action = scannername.nextLine();
 
-                        System.out.println("Small blind player, choose your move" );
-                        String action = scannername.nextLine();
-                        if (action.equals("raise")){
-                            System.out.println("Player " + 1 + " raises");
-                            //
-                            //call to the raises method for player
-                            //(playerlist.get((smallblind))).raise(); // Player actually means the object name of the small blind player (there are two seperate lists for this :Thinking:)
+
+                            if (action.equals("raise")){
+                                System.out.println("Player " + visualactiveP + " raises");
+                                //
+                                //call to the raises method for player
+
+                                (playerlist.get((activePlayer))).raise();
+                            }
+                            else if (action.equals("bet")){
+                                System.out.println("Player " + visualactiveP + " bets");
+                                //meaning giving money to the pit
+                                //call to bet method for player
+                                //Player.bet();
+                                (playerlist.get((activePlayer))).bet();
+                            }
+                            else if (action.equals("fold")){
+                                System.out.println("Player " + visualactiveP + " folds");
+                                //meaning that this player is not gonna bet and can not win the pot and will be skipped over during this hand
+                                //call to fold method for player
+                                //Player.fold();
+                                (playerlist.get((activePlayer))).fold();
+                            }
+                            else if (action.equals("all in")){
+                                System.out.println("Player " + visualactiveP + " goes all in");
+                                //meaning that this player is putting all their chips in the pit
+                                //call to all in method for player
+                                //Player.allin();
+                                (playerlist.get((activePlayer))).allin();
+                            }
+                            timesthrough = timesthrough + 1; //this is gonna be used to change the current player being called at
+                            if (timesthrough == 6){
+                                timesthrough = 1;
+                            }
+                            handloop = "yes";
+                            /*
+                                                        if (Player.hasplayerfolded.equals("yes")){ //check if all have folded
+                                areAllFolded = "yes";
+                            }
+                            if (Player.potmatch.equals("yes")){ //check if all have matched pot
+                                hasallmatchedpot = "yes";
+                            }
+                            if (Player.chipsLeft == 0){
+                                doPlayersStillHaveChips = "no";
+                            }
+                             */
+
+                            if (areAllFolded.equals("yes") || hasallmatchedpot.equals("yes") || areAllFolded.equals("yes") || doPlayersStillHaveChips.equals("no") ){ //A round of betting continues until every player has folded, put in all of their chips, or matched the amount put in by all other active players.
+                                handloop = "no";
+                                System.out.println("hand has ended");
+                            }
                         }
-                        else if (action.equals("bet")){
-                            System.out.println("Player " + 1 + " bets");
-                            //meaning giving money to the pit
-                            //call to bet method for player
-                            //Player.bet();
-                        }
-                        else if (action.equals("hold")){
-                            System.out.println("Player " + 1 + " holds");
-                            //meaning that this player is not gonna bet and can not win the pot and will be skipped over during this hand
-                            //call to hold method for player
-                            //Player.hold();
-                        }
-                        else if (action.equals("all in")){
-                            System.out.println("Player " + 1 + " goes all in");
-                            //meaning that this player is putting all their chips in the pit
-                            //call to all in method for player
-                            //Player.allin();
-                        }
-                        timesthrough = timesthrough + 1; //this is gonna be used to change the current player being called at
-                        handloop = "yes";
+
                         //call to dealing method where : private cards to each player > Small blind player one > Big blind one last > until each player has 2 cards > deck size changed (this should require an automated process and should not be ai driven unless we want to add the possibility of the dealer having dealing errors)
                         //enter bidding round
                         //at the end of hand..
@@ -129,7 +175,7 @@ public class Main {
 
     decide dealer > Big blind player set  > Small blind player set ✓+
     private cards to each player > Small blind player one > Big blind one last > until each player has 2 cards > deck size changed
-    bidding round > Small blind player (raise/bet half/ hold/all in) > Big blind (raise/bet/hold/all in) > everyone folds/player puts in all their chips/player matches amount by all other active players > bidding stops
+    bidding round > Small blind player (raise/bet half/ fold/all in) > Big blind (raise/bet/fold/all in) > everyone folds/player puts in all their chips/player matches amount by all other active players > bidding stops
     something happens
     Showdown > player's cards become public > winning hand player gets pit
     dealer changed > process restarts
@@ -158,7 +204,7 @@ public class Main {
     here is what the code should look like:
     a player is targeted to be the dealer and that dictates who is the small blind (the person who pays half the minimum) and the Big blind (the person who when bets pays the entire minimum or raise it and I think when they do they become the small blind)
     Card Class -                    Cards will have their own rules and properties for each card
-    Player Class value change -     player input (raise/bet/hold/all in/other actions) keep track of the betting for each player
+    Player Class value change -     player input (raise/bet/fold/all in/other actions) keep track of the betting for each player
                                     based on who has the highest bet change the target for the first choice of next turn
     Money pit value change-         players use their money/chips to add to the pit
     card Class reads hand -         finally the game's logic decides which player has the best hand using advanced intelligence techniques ((T H E   S H O W D O W N)I believe that this should be placed in the card class and it will read the values that corresponds to each player
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,14 +4,42 @@ import java.util.ArrayList; //needed for lists
 /**
  * Created by JIrvine on 4/12/2018.
  */
-public class Player {
-    int playername;
-
+public class Player extends Main{
+    Scanner scannername = new Scanner (System.in);
+    int playername; //no longer used
+    int PlayerID; // this would be used to identify the player's element value (these values will be declared as the Players are declared)
+    int PlayerStat; // this would be used to know what the player's current role is
+    String hasplayerfolded; // this will be used to declare if the defined player has folded
+    String potmatch; // this will be used to declare if the defined player has matched the amount each player put in the pot, if player is not active set this to be yes
+    String playeractive;
+    int chipsLeft;
     public Player(int playername){
-        playername = 1;
+        PlayerID = 1;
+        //PlayerStat = intplayerlist(PlayerID); //this is supposed to grab the number from the list in the main class but it isn't
+        hasplayerfolded = ("no");
+        chipsLeft = 100;
+        playeractive = "yes";
+        potmatch = "no";
 
     }
     public void test(){
+        System.out.println(playername);
+
+    }
+    public void raise(){
+        System.out.println(playername);
+
+    }
+    public void bet(){
+        System.out.println("how much?");
+        int bet = Integer.parseInt(scannername.nextLine());
+
+    }
+    public void fold(){
+        playeractive = "no";
+
+    }
+    public void allin(){
         System.out.println(playername);
 
     }
